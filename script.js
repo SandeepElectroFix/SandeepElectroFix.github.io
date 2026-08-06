8f98d3df6632548f086ab79f4a1faeec83c1e072
SandeepElectroFix <SandeepElectroFix@gmail.com>
2026-08-06 22:42:58 +0530

    Remove service worker registration logic
    Removed service worker registration code from script.js.

--- script.js
+++ script.js
@@ -191,11 +191,7 @@ ripple.remove();
 
 
 });
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("service-worker.js")
-      .then(() => console.log("Service Worker Registered"))
-      .catch(err => console.log("Service Worker Error:", err));
+
   });
 }
 /* Loader */
