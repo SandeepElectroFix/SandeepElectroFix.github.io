d4e3301bf7845a190b4485c326672d02b1b06f20
SandeepElectroFix <SandeepElectroFix@gmail.com>
2026-08-06 21:44:57 +0530

    Register service worker for app
    Added service worker registration for offline support.

--- script.js
+++ script.js
@@ -335,3 +335,12 @@ window.addEventListener("appinstalled", () => {
     }
     alert("🎉 Sandeep ElectroFix App Installed Successfully!");
 });
+/* ===== Register Service Worker ===== */
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("sw.js")
+        .then(() => console.log("Service Worker Registered"))
+        .catch(err => console.log(err));
+    });
+}
