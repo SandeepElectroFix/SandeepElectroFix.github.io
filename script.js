dedc7212798a90023bcf1561bb7b181313b750f5
SandeepElectroFix <SandeepElectroFix@gmail.com>
2026-08-06 09:57:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -191,3 +191,10 @@ ripple.remove();
 
 
 });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("service-worker.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch(err => console.log("Service Worker Error:", err));
+  });
+}
